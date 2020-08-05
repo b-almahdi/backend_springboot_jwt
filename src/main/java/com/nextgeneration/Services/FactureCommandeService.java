e18704649ec72d3c21558a16cb109c892d6723fc
b-almahdi <69243314+b-almahdi@users.noreply.github.com>
2020-08-05 10:56:01 +0000

    facture
    facture

---
+++ src/main/java/com/nextgeneration/Services/FactureCommandeService.java
@@ -0,0 +1,17 @@
+package com.nextgeneration.Services;
+
+import org.springframework.stereotype.Service;
+
+import com.nextgeneration.dtos.CommandeFactureLivraisonDTO;
+
+@Service
+public class FactureCommandeService {
+
+	public Object generateFactureForCommande(CommandeFactureLivraisonDTO commandeFactureLivraisonDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
